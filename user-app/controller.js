63c9c699bdade166a8159994ffe9d535db5d4f84
Sakshee <sakshee1120@gmail.com>
2021-02-04 00:31:00 +0530

    ipay

--- user-app/controller.js
+++ user-app/controller.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const { User, Event, Register, Update, Teams, Leaderboard, Sponsors } = require('./model');
 var nodemailer = require('nodemailer');
-const { response, request } = require('express');
+const { response, request, application, json } = require('express');
 
 const ROLE = {
     BASIC: 'basic',
@@ -20,41 +20,45 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+
 payment = async (req, res) => {
 
     if(req.method === 'POST') {
         try{
 
-            console.log(request.body)
-            
-            axios({
-                method: "post",
-                url: 'https://public_key:secret_key@api.ippopay.com/v1/order/create',
-                body: {
-                    "amount": req.body.amount,
-                    "currency": "INR",
-                    "payment_modes": "cc,dc,nb,upi" ,
-                    "customer": {
-                      "name": req.body.username,
-                      "email": req.body.email,
-                      "phone": {
-                            "country_code": 91 ,
-                            "national_number": req.body.phoneno
-                        }
-                      }
-                },
-                auth: {
-                    username: 'pk_test_PMTwU39TjbUC',
-                    password: 'sk_test_UYkRAbTh1082YnTIIFNy1NMgBUafLbTZ2tzoRhru'
-                  }
-              }).then((response)=>{
-                  //alert(JSON.stringify(response.data))
-                  console.log("API call success " + JSON.stringify(response.data))
-              }).catch((error)=>{
-                  console.log("Error in payment" + error)
-              })
+            console.log(req.body)
 
-              res.status(200).json(response.data);
+            const request = require('request');
+            var username = process.env.IPPO_TEST_PUBLIC;
+            var password = process.env.IPPO_TEST_SECRET;
+            var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+            var data = {
+                "amount": req.body.amount,
+                "currency": "INR",
+                "payment_modes": "cc,dc,nb,upi" ,
+                "customer": {
+                "name": req.body.username,
+                "email": req.body.email,
+                "phone": {
+                        "country_code": "91" ,
+                        "national_number": req.body.phoneno
+                    }
+                }
+            }
+            var options = {
+                url : 'https://api.ippopay.com/apitest/v1/pg/order/create',
+                headers:{
+                    'Content-Type': 'application/json',
+                    "Authorization" : auth
+                },
+              body:JSON.stringify(data)
+            }
+            request.post(options , function (error, response, body ) {
+                res.status(200).json(JSON.parse(response.body))
+                console.log(body)
+            })
+
+            
 
         } catch (err) {
             res.status(500).json({message: `Internal server error : ${err.message}`});
@@ -399,7 +403,6 @@ leaderboard = async (req, res) => {
                college: req.body.college,
                score: req.body.score
            })
-
            var waitedscore = await score.save();
             res.json(waitedscore).status(200);
 */
